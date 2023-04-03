89f92c138b2f91825be8724d814fb883ce53e720
Augustine Nguyen <tri2401na@gmail.com>
2023-04-03 18:25:16 -0500

    added webcam

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-     return '<h1>Hello World<h1>'
+def main_page():
+     return render_template('index.html')
+
+@app.route('/astro_cam')
+def astro_cam():
+     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen_frames():
+     camera = cv2.VideoCapture(0)
+     while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
