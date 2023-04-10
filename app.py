b9a85466933d9044d89e37d2899be8cff95c0f40
roddur99 <73806645+roddur99@users.noreply.github.com>
2023-04-10 14:57:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, Response
 import cv2
+import numpy as np
 
 app = Flask(__name__)
+
+from  hubconf import custom
+model = custom(path_or_model='droned13k.pt')
+#model = custom(path_or_model='yolov7.pt')
+model.conf = 0.5
+
+#cap = cv2.VideoCapture(1)
+#cap.set(cv2.CAP_PROP_EXPOSURE, 1000)
 
 @app.route('/')
 def main_page():
@@ -12,13 +21,19 @@ def astro_cam():
      return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def gen_frames():
-     camera = cv2.VideoCapture(0)
+     cap = cv2.VideoCapture(1)
+     cap.set(cv2.CAP_PROP_EXPOSURE, 500)
      while True:
-        success, frame = camera.read()  # read the camera frame
+        success, frame = cap.read()  # read the camera frame
+
+        results = model(frame)
+        f2 = np.squeeze(results.render())     
+
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv2.imencode('.jpg', f2)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+     
